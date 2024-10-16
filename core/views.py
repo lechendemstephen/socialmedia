95925f98ddfa3e4bc5f87512809132c9752a05e0
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2024-10-15 22:32:00 -0400

    user profile setup

--- core/views.py
+++ core/views.py
@@ -13,9 +13,39 @@ def index(request):
 
 @login_required(login_url='signin')
 def settings(request): 
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST": 
+
+        if request.FILES.get('image') == None: 
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio 
+            user_profile.location = location 
+            user_profile.save()
+        
+        if request.FILES.get('image') != None: 
+            image =  request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio 
+            user_profile.location = location 
+            user_profile.save()
+
+        return redirect('settings')
+        
+    context = {
+        "user_profile": user_profile, 
+    }
 
 
-    return render(request, 'pages/setting.html')
+
+    return render(request, 'pages/setting.html', context)
 
 
 
